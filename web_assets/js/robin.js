b950d70424123a7890d750d69adf8e99b3413fff
Paul Huxen <phuxen@gmail.com>
2014-03-26 09:18:24 +0000

    Initial Commit

---
+++ web_assets/js/robin.js
@@ -0,0 +1,22 @@
+document.createElement("article");  
+document.createElement("footer");  
+document.createElement("header");  
+document.createElement("hgroup");  
+document.createElement("nav");
+
+$(document).ready(function($){
+	
+	$('.iosslider').each(function() {
+	    $(this).iosSlider({
+	        scrollbar: false,
+			snapToChildren: true,
+			desktopClickDrag: true,
+			responsiveSlideWidth: true,
+			navNextSelector: $('.sliderNext'),
+			navPrevSelector: $('.sliderPrev'),
+			infiniteSlider: false,
+			keyboardControls: true	    
+		});	
+	});
+	
+});
